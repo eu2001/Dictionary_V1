0645d4141cb1710edb83bba2bc0c1c7997ec919e
Eric <Eric>
2023-02-04 11:53:39 -0500

    Commit basic project

---
+++ Dictionary_V1/Model/Dictionary.swift
@@ -0,0 +1,32 @@
+//
+//  Dictionary.swift
+//  Dictionary_V1
+//
+//  Created by Eric Sousa on 2/3/23.
+//
+
+import Foundation
+
+struct Book : Decodable, Identifiable {
+    
+    var id = 1
+    var chapter =  1
+        var nameEng = "Good Morning"
+          var  audioEng = "wwww.youtube.com"
+           var translitEng =  "Good Morning"
+          var  namePort = "Bom dia"
+           var translitPor = "bong jee-uh"
+            var audioPor =  "wwww.facebook.com"
+            var nameEsp =  "Buenos dias"
+            var translitEsp =  "boo-e-nos dee-ahs"
+            var audioEsp = "wwww.instagram.com"
+            var nameIta =  "Buongiorno"
+            var translitIta =  "boo-ong-jee-ohr-noh"
+            var audioIta =  "wwww.globo.com"
+            var nameGer = "Guten Morgen"
+            var translitGer = "goo-t'en mohr-g'en"
+            var audioGer =  "www.cnn.com"
+            var nameFre =  "Bonjour"
+            var translitFre = "bong-joor"
+            var audioFre = "www.uol.com.br"
+}
